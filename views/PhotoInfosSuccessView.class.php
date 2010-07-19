539f415a9fd83501c0095f458a9ad9e7b16ba1d4
Franck STAUFFER <franck.stauffer@rbs.fr>
2010-07-19 09:18:51 +0200

    [CLEANUP]  Removed useless call to setAttribute

--- views/PhotoInfosSuccessView.class.php
+++ views/PhotoInfosSuccessView.class.php
@@ -8,7 +8,6 @@ class photoalbum_PhotoInfosSuccessView extends f_view_BaseView
 	public function _execute($context, $request)
 	{
 		$this->setTemplateName('Photoalbum-Block-Photo-Infos', K::HTML);
-		$this->setAttribute('item', $request->getAttribute('item'));
 		$this->setAttribute('album', $request->getAttribute('album'));
 	}
 }
